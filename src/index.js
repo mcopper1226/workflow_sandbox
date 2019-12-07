3516523d80c8ee3b1f949092ca52ca07e96152ed
Megan Copper <mni7a@S-MNI7A-MBP.local>
2019-12-07 12:49:31 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('app is running!');
+});
+
+app.get('/mongo', (req, res) => {
+  res.send(`mongo url set from cli: ${process.env.MONGODB_URI}`);
+});
+
+app.listen(port, () => {
+  console.log(`REST API on http://localhost:${port}`);
+});
